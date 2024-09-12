57d6f0dfd19fff86feaef891af85dc9218bfec05
Shaleen <shaleen@Shaleens-Laptop.local>
2024-09-12 10:56:42 +0530

    changes done in development branch

--- index.js
+++ index.js
@@ -9,3 +9,5 @@ var server = http.createServer(function (req, res) {   // 2 - creating server
 server.listen(5000); //3 - listen for any incoming requests
 
 console.log('Node.js web server at port 5000 is running..')
+
+console.log("changes are now followed into the development branch to see conflicts")
