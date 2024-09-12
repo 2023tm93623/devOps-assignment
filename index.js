26f3ccc39ff7a99cb1ab19d84e0d8f963633691b
Shaleen <shaleen@Shaleens-Laptop.local>
2024-09-12 11:02:06 +0530

    changes done to dev

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var server = http.createServer(function (req, res) {   // 2 - creating server
 
 });
 
+
 server.listen(5000); //3 - listen for any incoming requests
 
 console.log('Node.js web server at port 5000 is running..')
